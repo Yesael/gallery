d9088c63a743a4e81b8c210c68673571388ac50b
Yesael <61566320+Yesael@users.noreply.github.com>
2021-11-29 19:02:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const sliderContainer = document.querySelector(".slider-container");
-const slideRight = document.querySelector(".right-slide");
-const slideLeft = document.querySelector(".left-slide");
-const upButton = document.querySelector(".up-button");
-const downButton = document.querySelector(".down-button");
-const slidesLength = slideRight.querySelectorAll("div").length;
+let panels = document.querySelectorAll(".panel");
 
-let activeSlideIndex = 0;
+function f() {
+	this.classList.toggle("open");
+}
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
-
-const changeSlide = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight;
-  if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex > slidesLength - 1) activeSlideIndex = 0;
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) activeSlideIndex = slidesLength - 1;
-  }
-  slideRight.style.transform = `translateY(-${
-    activeSlideIndex * sliderHeight
-  }px)`;
-  slideLeft.style.transform = `translateY(${
-    activeSlideIndex * sliderHeight
-  }px)`;
-};
-
-upButton.addEventListener("click", () => changeSlide("up"));
-downButton.addEventListener("click", () => changeSlide("down"));
+function openActive(event) {
+	if (event.propertyName.includes("flex")) {
+		this.classList.toggle("open-active");
+	}
+}
+panels.forEach((panel) => panel.addEventListener("click", f));
+panels.forEach((panel) => panel.addEventListener("transitionend", openActive));
